112afd940594cef387e018fabdef14e13d49dfe0
Chie Stroud <urirunrunkoko@gmail.com>
2021-07-17 13:28:06 -0500

    Add project files.

---
+++ Letter Loops/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Letter_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter strings like abcd");
+            var letters = Console.ReadLine();
+            var counter = 1;
+            string newString = "";
+
+            foreach (var letter in letters)
+            {
+                for (var i = 0; i < counter; i++)
+                {
+                    newString += letter;
+                }
+                newString += "-";
+                counter++;
+            }
+            Console.WriteLine(newString.TrimEnd('-'));
+        }
+    }
+}
