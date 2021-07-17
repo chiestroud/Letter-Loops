8e1bb25fc1313bb6eaf9d27d4e95fb7d2e9776bc
Chie Stroud <urirunrunkoko@gmail.com>
2021-07-17 15:11:30 -0500

    Updated solutions

--- Letter Loops/Program.cs
+++ Letter Loops/Program.cs
@@ -6,21 +6,32 @@ namespace Letter_Loops
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter strings like abcd");
-            var letters = Console.ReadLine();
-            var counter = 1;
-            string newString = "";
-
-            foreach (var letter in letters)
+            Console.WriteLine("Please enter a word to loop through");
+            var testString = Console.ReadLine();
+            var counter = 0;
+            var finalString = "";
+            for (var i = 0; i < testString.Length; i++)
             {
-                for (var i = 0; i < counter; i++)
-                {
-                    newString += letter;
-                }
-                newString += "-";
                 counter++;
+                for (var j = 0; j < counter; j++)
+                {
+                    if (j == 0 && i > 0)
+                    {
+                        var first = char.ToUpper(testString[i]);
+                        finalString += $"-{first}";
+                    }
+                    else if (i == 0)
+                    {
+                        var veryFirst = char.ToUpper(testString[i]);
+                        finalString += veryFirst;
+                    }
+                    else
+                    {
+                        finalString += $"{testString[i]}";
+                    }
+                }
             }
-            Console.WriteLine(newString.TrimEnd('-'));
+            Console.WriteLine($"{finalString}");
         }
     }
 }
